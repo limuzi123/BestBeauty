e00e9ea3b5331044333274d24207d0531a080c24
MiXiuZhuZhu <18340922875@163.com>
2016-09-03 11:18:28 +0800

    SetActivity清除缓存功能

--- MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/activity/mine/SetActivity.java
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/activity/mine/SetActivity.java
@@ -1,12 +1,19 @@
 package com.lanou3g.mostbeauty.activity.mine;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 import com.lanou3g.mostbeauty.R;
 import com.lanou3g.mostbeauty.base.BaseActivity;
+import com.lanou3g.mostbeauty.base.MyApp;
+import com.lanou3g.mostbeauty.other.DataClean;
+
+import java.io.File;
 
 /**
  * Created by dllo on 16/9/2.
@@ -15,6 +22,7 @@ import com.lanou3g.mostbeauty.base.BaseActivity;
 public class SetActivity extends BaseActivity implements View.OnClickListener{
     private ImageView imageViewSetBack;
     private RelativeLayout relativeLayoutFeedBack,relativeLayoutSetHead;
+    private RelativeLayout relativeLayoutClean;
 
     @Override
     protected int getLayout() {
@@ -29,6 +37,8 @@ public class SetActivity extends BaseActivity implements View.OnClickListener{
         imageViewSetBack.setOnClickListener(this);
         relativeLayoutFeedBack = (RelativeLayout) findViewById(R.id.relative_feedback);
         relativeLayoutFeedBack.setOnClickListener(this);
+        relativeLayoutClean = (RelativeLayout) findViewById(R.id.relative_layout_clean);
+        relativeLayoutClean.setOnClickListener(this);
 
     }
 
@@ -36,7 +46,26 @@ public class SetActivity extends BaseActivity implements View.OnClickListener{
     protected void initData() {
 
     }
-
+    private void Dialog(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(SetActivity.this);
+        builder.setMessage("确认清除缓存吗?");
+        builder.setTitle("提示");
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+               // DataClean.getCacheSize();
+                DataClean.cleanInternalCache(SetActivity.this);
+                Toast.makeText(SetActivity.this, "清除了缓存数据", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+        builder.create().show();
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -49,6 +78,8 @@ public class SetActivity extends BaseActivity implements View.OnClickListener{
             case R.id.relative_feedback:
                 startActivity(new Intent(this,FeedbackActivity.class));
                 break;
+            case R.id.relative_layout_clean:
+                Dialog();
         }
     }
 }
