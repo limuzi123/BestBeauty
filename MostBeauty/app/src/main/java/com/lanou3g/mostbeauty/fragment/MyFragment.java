23a41e1a8c53fc0af508a602cc6697d6bd79bc7a
MiXiuZhuZhu <18340922875@163.com>
2016-09-05 14:31:52 +0800

    ...

--- MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/MyFragment.java
+++ MostBeauty/app/src/main/java/com/lanou3g/mostbeauty/fragment/MyFragment.java
@@ -1,8 +1,13 @@
 package com.lanou3g.mostbeauty.fragment;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ImageView;
+import android.widget.PopupWindow;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -23,9 +28,10 @@ import com.lanou3g.mostbeauty.base.BaseFragment;
  * Created by dllo on 16/8/30.
  */
 public class MyFragment extends BaseFragment implements View.OnClickListener {
-    private ImageView imageViewMyHead,imageViewSet;
+    private ImageView imageViewMyHead,imageViewSet,imageViewSina,imageViewQQ,imageViewWeixin;
     private TextView textViewFeedBack,textViewdraw;
     private RelativeLayout relativeLayoutAttention,relativeLayoutWish;
+    private PopupWindow mPop;
 
     @Override
     protected int initLayout() {
@@ -52,7 +58,45 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
     protected void initData() {
 
     }
+    private PopupWindow LogonPop(){
+        mPop = new PopupWindow();
+        View popView = LayoutInflater.from(getActivity()).inflate(R.layout.my_pop,null);
+        imageViewSina = (ImageView) popView.findViewById(R.id.sina_share);
+        imageViewSina.setOnClickListener(this);
+        imageViewQQ = (ImageView) popView.findViewById(R.id.image_qq);
+        imageViewQQ.setOnClickListener(this);
+        imageViewWeixin = (ImageView) popView.findViewById(R.id.image_weixin);
+        imageViewWeixin.setOnClickListener(this);
+        mPop.setWidth(800);
+        mPop.setHeight(500);
+        //点击外部是否消失
+        mPop.setOutsideTouchable(true);
+        //设置焦点
+        mPop.setFocusable(true);
+        mPop.setContentView(popView);
+        Drawable drawable = new ColorDrawable(0xffffff);
+        mPop.setBackgroundDrawable(drawable);
+        mPop.setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
+        mPop.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_ADJUST_RESIZE);
+        backgroundAlpha(1f);
+        mPop.setOnDismissListener(new PopupWindow.OnDismissListener() {
+            @Override
+            public void onDismiss() {
+                backgroundAlpha(1f);
+            }
+        });
+        return mPop;
+    }
+    public void backgroundAlpha(float bgAlpha) {
+        //设全屏
+        WindowManager.LayoutParams lp = getActivity().getWindow().getAttributes();
+        lp.alpha = bgAlpha;
+        this.getActivity().getWindow().addFlags(WindowManager.LayoutParams.FLAG_DIM_BEHIND);
+        this.getActivity().getWindow().setAttributes(lp);
+    }
+    private void MyLogon(){
 
+    }
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
@@ -76,4 +120,5 @@ public class MyFragment extends BaseFragment implements View.OnClickListener {
                 break;
         }
     }
+
 }
